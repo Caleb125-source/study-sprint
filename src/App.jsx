a5b81334a33c14b8724a224247615f35276d5b65
CalebMunyoki <munyokicaleb@gmail.com>
2026-01-26 13:54:58 +0300

    Set up routing with Routes and pass task management props to PlannerPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { PlannerPage } from './components/PlannerPage'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (task) => {
+    setTasks((prevTasks) => [...prevTasks, task])
+  }
+
+  const updateTask = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, status: "Done" } : task
+      )
+    )
+  }
+
+  const deleteTask = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId))
+  }
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Routes>
+        <Route
+        path="/"
+        element={
+          <PlannerPage
+            tasks={tasks}
+            addTask={addTask}
+            updateTask={updateTask}
+            deleteTask={deleteTask}
+          />
+        }
+      />
+      </Routes>
+      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 

c88db54dec3e8b8b9badcd4799d73ff7d251d820
CalebMunyoki <munyokicaleb@gmail.com>
2026-01-29 22:55:29 +0300

    import components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import Layout from "./components/Layout";
 
 import LandingHomePage from "./pages/LandingHomePage";
 import FeaturesPage from "./pages/FeaturesPage";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import PlannerPage from "./components/PlannerPage";
+import TimerPage from "./components/TimerPage";
+import ProgressPage from "./pages/ProgressPage";
+import SettingsPage from "./pages/SettingsPage";
 
-// temporary placeholders (until teammates finish)
-const AboutPage = () => <h2>About Page</h2>;
-const HomePage = () => <h2>Dashboard</h2>;
-const PlannerPage = () => <h2>Planner</h2>;
-const TimerPage = () => <h2>Timer</h2>;
-const ProgressPage = () => <h2>Progress</h2>;
-const SettingsPage = () => <h2>Settings</h2>;
 
 function App() {
   return (
@@ -18,7 +17,10 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<LandingHomePage />} />
         <Route path="/features" element={<FeaturesPage />} />
-        <Route path="/dashboard" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/planner" element={<PlannerPage />} />
+        <Route path="/timer" element={<TimerPage />} />
         <Route path="/progress" element={<ProgressPage />} />
         <Route path="/settings" element={<SettingsPage />} />
       </Route>

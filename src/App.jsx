e76a05410ace496f7396e5addb5cb5dcea77a88e
CalebMunyoki <munyokicaleb@gmail.com>
2026-01-30 16:24:53 +0300

    Pass theme and setTheme props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import { useState, useEffect } from "react"
 
 import LandingHomePage from "./pages/LandingHomePage";
 import FeaturesPage from "./pages/FeaturesPage";
@@ -12,6 +13,13 @@ import SettingsPage from "./pages/SettingsPage";
 
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.body.classList.remove("light", "dark");
+    document.body.classList.add(theme);
+  }, [theme])
+
   return (
     <Routes>
       <Route element={<Layout />}>
@@ -20,9 +28,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/planner" element={<PlannerPage />} />
-        <Route path="/timer" element={<TimerPage />} />
+        <Route path="/timer" element={<TimerPage tasks={[]} />} />
         <Route path="/progress" element={<ProgressPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/settings" element={<SettingsPage theme={theme} setTheme={setTheme} />} />
       </Route>
     </Routes>
   );

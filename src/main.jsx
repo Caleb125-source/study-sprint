4bedfc13e279cff5170cbda38887070c1bad07bf
CalebMunyoki <munyokicaleb@gmail.com>
2026-01-30 16:26:25 +0300

    import SessionProvider from SessionsContext.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";  
 import { SettingsProvider } from "./context/SettingsContext.jsx";
+import { SessionsProvider } from "./context/SessionsContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
     <SettingsProvider>
+      <SessionsProvider>
       <App />
+      </SessionsProvider>
     </SettingsProvider>
     </BrowserRouter>
   </React.StrictMode>

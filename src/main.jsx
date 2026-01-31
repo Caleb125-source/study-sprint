1a2ead27db5246dfb65ea903278ef0eccb2cfd95
Jburgei <joyburgei4@gmail.com>
2026-01-31 21:12:37 +0300

    updated main to ensure sessions provider was being accessed correctly

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+
 import { SettingsProvider } from "./context/SettingsContext.jsx";
 import { SessionsProvider } from "./context/SessionsContext.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
@@ -10,13 +11,13 @@ import { AuthProvider } from "./auth/AuthContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <SettingsProvider>
-        <SessionsProvider>
-          <AuthProvider>
+      <AuthProvider>
+        <SettingsProvider>
+          <SessionsProvider>
             <App />
-          </AuthProvider>
-        </SessionsProvider>
-      </SettingsProvider>
+          </SessionsProvider>
+        </SettingsProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );

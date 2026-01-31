e33c0f7c723002777cdcdaf3e999017533817ecc
Jburgei <joyburgei4@gmail.com>
2026-01-31 13:22:35 +0300

    modified main to include AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";  
+import "./index.css";
 import { SettingsProvider } from "./context/SettingsContext.jsx";
 import { SessionsProvider } from "./context/SessionsContext.jsx";
+import { AuthProvider } from "./auth/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <SettingsProvider>
-      <SessionsProvider>
-      <App />
-      </SessionsProvider>
-    </SettingsProvider>
+      <SettingsProvider>
+        <SessionsProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </SessionsProvider>
+      </SettingsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
